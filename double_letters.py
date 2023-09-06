91b71bf78ecab7234faed1ded64afc025520a833
Karyott <130049376+Karyott@users.noreply.github.com>
2023-09-06 22:02:06 +0800

    Add files via upload

---
+++ double_letters.py
@@ -0,0 +1,13 @@
+#Double Letters
+
+#The goal of this challenge is to analyze a string to check if it contains two of the same letter in a row. For example, the string "hello" has l twice in a row, while the string "nono" does not have two identical letters in a row.
+
+def double_letters(string):
+    
+    for i in range(len(string)-1):
+           if string[i] == string[i+1]:
+              return True
+        
+    return False 
+
+print(double_letters("nono"))
